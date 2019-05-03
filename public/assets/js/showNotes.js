7bdb09d0a8b81faa4fb7be67ea1131fb6bc971c1
JC <james.calingo@gmail.com>
2019-05-03 09:54:27 -0400

    moment added

--- public/assets/js/showNotes.js
+++ public/assets/js/showNotes.js
@@ -1,17 +1,21 @@
+
+
 const getAndRenderNotes = function() {
   $.ajax({ method: "GET", url: "/api/notes" }).then(function(data) {
     var notesArray = [];
 
     for (var i = 0; i < data.length; i++) {
       var note = data[i];
+     var dateTime = moment(note.created_at).format("MM/DD/YYYY h:mm a")
 
       var $newNote = $("<div>");
       var $noteTitle = $("<h5>").text(note.title);
       var $noteBody = $("<p>").text(note.body);
-      var $noteTime = $("<p>").text(`Created at ${note.created_at}`);
-      var $del = 
+      var $noteTime = $("<p>").text(`Created at ${dateTime}`);
+      var $del = $("<button>").text("Delete this Note")
+      $del.addClass("btn-danger btn mb-5 delete")
 
-      $newNote.append($noteTitle, $noteBody, $noteTime);
+      $newNote.append($noteTitle, $noteBody, $noteTime, $del);
 
       notesArray.push($newNote);
     }
@@ -21,3 +25,7 @@ const getAndRenderNotes = function() {
 };
 
 getAndRenderNotes();
+
+$(".delete").on("click", function(){
+  $ajax({})
+})
