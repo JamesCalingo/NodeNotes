4041005e9144d7947ca7e41f2944786957a6d390
JC <james.calingo@gmail.com>
2021-08-10 11:19:16 -0400

    changing local port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var htmlRoutes = require('./routes/htmlRoutes')
 // Tells node that we are creating an "express" server
 var app = express()
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 3000
+var PORT = process.env.PORT || 4000
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }))
